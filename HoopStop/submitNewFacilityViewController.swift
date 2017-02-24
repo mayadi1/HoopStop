d6fb04c52d33777f5c35c266bac9d408df19495d
mayadi1 <admin@ayadios.com>
2017-02-24 00:12:13 -0800

    newFacilityVC update

--- HoopStop/submitNewFacilityViewController.swift
+++ HoopStop/submitNewFacilityViewController.swift
@@ -7,16 +7,35 @@
 //
 
 import UIKit
+import MapKit
 
-class submitNewFacilityViewController: UIViewController,UITextViewDelegate,UITextFieldDelegate {
+class submitNewFacilityViewController: UIViewController,UITextViewDelegate,UITextFieldDelegate,MKMapViewDelegate {
+    @IBOutlet weak var facilityName: UITextField!
+    @IBOutlet weak var streetAddress: UITextField!
+    @IBOutlet weak var city: UITextField!
+    @IBOutlet weak var state: UITextField!
+    @IBOutlet weak var zip: UITextField!
     
     @IBOutlet weak var addPhotoImage: UIImageView!
     @IBOutlet weak var additionalFacilityInfo: UITextView!
-    
+    var locationManager = CLLocationManager()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.hideKeyboardWhenTappedAround()
         additionalFacilityInfo.delegate = self
+        if (CLLocationManager.locationServicesEnabled()){
+        if CLLocationManager.authorizationStatus() == .notDetermined {
+            self.locationManager.requestWhenInUseAuthorization()
+        }
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+        setAddress()
+        }else{
+            #if debug
+                println("Location services are not enabled");
+            #endif
+        }
     }
     
     override func didReceiveMemoryWarning() {
@@ -47,6 +66,40 @@ class submitNewFacilityViewController: UIViewController,UITextViewDelegate,UITex
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         self.view.endEditing(true)
         return false
+    }
+    
+    func setAddress(){
+        let geoCoder = CLGeocoder()
+        let location = CLLocation(latitude: (locationManager.location?.coordinate.latitude)!, longitude: (locationManager.location?.coordinate.longitude)!)
+        geoCoder.reverseGeocodeLocation(location, completionHandler: { (placemarks, error) -> Void in
+            // Place details
+            var placeMark: CLPlacemark!
+            placeMark = placemarks?[0]
+            // Address dictionary
+            // Location name
+            if let locationName = placeMark.addressDictionary!["Name"] as? NSString {
+                self.facilityName.text = locationName as String
+            }
+            // Street address
+            if let street = placeMark.addressDictionary!["Thoroughfare"] as? NSString {
+                self.streetAddress.text = street as String
+            }
+            // City
+            if let city = placeMark.addressDictionary!["City"] as? NSString {
+                self.city.text = city as String
+            }
+            // Zip code
+            if let zip = placeMark.addressDictionary!["ZIP"] as? NSString {
+                self.zip.text = zip as String
+            }
+            // state
+            if let state = placeMark.addressDictionary!["State"] as? NSString {
+                self.state.text = state as String
+            }
+            // Country
+            //if let country = placeMark.addressDictionary!["Country"] as? NSString {
+            //}
+        })
     }
 }
 
