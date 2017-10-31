71dd6eb29b799ab52dbc1924783d08eca99bf1f7
mayadi1 <admin@ayadios.com>
2017-11-01 00:25:44 +0100

    Including DataTimePicker

--- HoopStop/ShowPinInfoViewController.swift
+++ HoopStop/ShowPinInfoViewController.swift
@@ -370,15 +370,35 @@ class ShowPinInfoViewController: UIViewController, UITableViewDelegate,UITableVi
     //self.navItem.title
     //[users[indexPath.row]]
     @IBAction func inviteBtnPressed(_ sender: Any) {
-        print("iam pressed")
-        
+        pickDateTime()
+    }
+    
+    func pickDateTime()
+    {
+        let min = Date().addingTimeInterval(-60 * 60 * 24 * 4)
+        let max = Date().addingTimeInterval(60 * 60 * 24 * 4)
+        let picker = DateTimePicker.show(selected: Date(), minimumDate: min, maximumDate: max)
+        picker.highlightColor = UIColor(red: 255.0/255.0, green: 138.0/255.0, blue: 138.0/255.0, alpha: 1)
+        picker.darkColor = UIColor.darkGray
+        picker.doneButtonTitle = "!! DONE DONE !!"
+        picker.todayButtonTitle = "Today"
+        picker.is12HourFormat = true
+        picker.dateFormat = "hh:mm aa dd/MM/YYYY"
+        //        picker.isTimePickerOnly = true
+        picker.completionHandler = { date in
+            let formatter = DateFormatter()
+            formatter.dateFormat = "hh:mm aa dd/MM/YYYY"
+           // self.title = formatter.string(from: date)
+            
             for index in self.tickedIndexPaths{
-                print(users[index.row].name!)
-                users[index.row].invitedAt.append(self.navItem.title!)
-                usersFef.child(users[index.row].userUid!).child("invitedAt").setValue(users[index.row].invitedAt)
+                print(self.users[index.row].name!)
+                self.users[index.row].invitedAt.append(self.navItem.title!)
+                self.usersFef.child(users[index.row].userUid!).child("invitedAt").setValue(users[index.row].invitedAt)
                 SVProgressHUD.showSuccess(withStatus: "Invite sent to " + "\(self.tickedIndexPaths.count)")
                 self.tableView.reloadData()
             }
+
+        }
     }
     
 }
